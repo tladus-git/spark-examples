fab1b08a67b1cfdfbdc9f4058cfb37053226c850
paul <paul.no@nexr.com>
2021-10-27 12:31:57 +0900

    - Spark Action Test App

---
+++ src/main/java/com/nexr/spark/action/app/JavaSparkPi.java
@@ -0,0 +1,46 @@
+package com.nexr.spark.action.app;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaSparkPi {
+    private static final Logger logger = LoggerFactory.getLogger(JavaSparkPi.class);
+
+    public static void main(String[] args) throws Exception {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaSparkPi")
+                .getOrCreate();
+
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
+
+        int slices = (args.length == 1) ? Integer.parseInt(args[0]) : 2;
+        int n = 100000 * slices;
+        List<Integer> l = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+
+        JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+        int count = dataSet.map(integer -> {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            return (x * x + y * y <= 1) ? 1 : 0;
+        }).reduce((integer, integer2) -> integer + integer2);
+
+        double pi = 4.0 * count / n;
+
+        logger.info("spark-app-result= Pi is roughly " + pi);
+        System.out.println("Stdout > spark-app-result= Pi is roughly " + pi);
+        logger.info("Pi is roughly {} ", pi);
+
+        spark.stop();
+    }
+}
